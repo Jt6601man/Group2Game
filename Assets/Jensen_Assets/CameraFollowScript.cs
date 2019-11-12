d2af166bbd6a35c5fdbfc609029b7e8b6f1f8da9
Jt6601man <jensen.tucker@gmail.com>
2019-11-12 16:12:23 -0500

    Added Skeleton for Player and Movement

---
+++ Assets/Jensen_Assets/CameraFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+    public GameObject followObject;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (followObject)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(followObject.transform.position.x, followObject.transform.position.y, transform.position.z), .1f);
+        }
+    }
+}
---
+++ Assets/Jensen_Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5, walkSpeed = 5;
+
+    float localScaleXP2;
+    float localScaleYP2;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+    void Start()
+    {
+        
+        localScaleXP2 = transform.localScale.x;
+        localScaleYP2 = transform.localScale.y;
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        playerMoving = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+        float horMovementP2 = Input.GetAxis("Horizontal") * speed;
+        float vertMovementP2 = Input.GetAxis("Vertical") * speed;
+
+        Rigidbody2D rb2 = GetComponent<Rigidbody2D>();
+        rb2.velocity = new Vector3(horMovementP2, vertMovementP2, 0);
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+
+    }
+}
