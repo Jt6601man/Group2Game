40d963e32211ff2ee2454bbd0e97b3e8e23d67a4
alexisBelt <57182633+alexisBelt@users.noreply.github.com>
2019-11-17 20:59:50 -0500

    Getting the Cannon to Turn

---
+++ Assets/Alexis_Assets/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+    public Transform playerTarget;
+
+    Transform cannon;
+
+    void Start()
+    {
+        cannon = GetComponent<Boss>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = (playerTarget.position - transform.position);
+        delta.z = 0;
+        cannon.transform.up = delta;
+    }
+}
