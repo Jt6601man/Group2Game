3d0d1e1a426f7d425cb0918f77a001454c53b79f
alexisBelt <57182633+alexisBelt@users.noreply.github.com>
2019-11-18 01:15:37 -0500

    Working on Cannon Movement
    the cannon turns, but I haven't finished making it move yet

--- Assets/Alexis_Assets/Boss.cs
+++ Assets/Alexis_Assets/Boss.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
 
-    public Transform playerTarget;
+    public GameObject playerTarget;
 
     Transform cannon;
+    public List<Vector2> waypoints = new List<Vector2>();
 
     void Start()
     {
@@ -17,8 +18,14 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 delta = (playerTarget.position - transform.position);
+        Vector3 delta = (playerTarget.transform.position - transform.position);
         delta.z = 0;
         cannon.transform.up = delta;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        foreach (Vector2 v in waypoints)
+            Gizmos.DrawSphere((Vector3)v, .25f);
     }
 }
