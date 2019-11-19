86d797ecddbbbdd2273a66d57928f535be8dcc99
David <wills55@marshall.edu>
2019-11-18 19:46:06 -0500

    Main Menu Scene and Script

---
+++ Assets/David_Assets/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+    public string sceneToLoad;
+    public GameObject aboutCanvas;
+
+    void Start()
+    {
+        aboutCanvas.SetActive(false);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+
+    }
+
+    public void About()
+    {
+        aboutCanvas.SetActive(true);
+    }
+
+    public void Back()
+    {
+        aboutCanvas.SetActive(false);
+    }
+
+
+}
