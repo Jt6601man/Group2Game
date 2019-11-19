11eb82b0595d93ba0f38cdf48fd4aab2adb19e1b
alexisBelt <57182633+alexisBelt@users.noreply.github.com>
2019-11-18 21:42:23 -0500

    Rotation and Moving Finished!
    Cannon rotates, moves with coroutine, and I started on the health and health bar.

--- Assets/Alexis_Assets/Boss.cs
+++ Assets/Alexis_Assets/Boss.cs
@@ -1,33 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
 
     public GameObject playerTarget;
     public float speed = 2;
+    public float gunDamage = 3;
+    public float swordDamage = 1;
+    public float maxHealth = 30;
+    public GameObject sword;
 
+    float currentHealth;
     Transform cannon;
     Vector2 currentGizmo;
     int currentIndex = 0;
+    //GameObject gun;
+    Slider healthBar;
+
 
     void Start()
     {
+        currentHealth = maxHealth;
         cannon = GetComponent<Boss>().transform;
-        UpdateCurrentGizmo();
+        //gun = ;
+        healthBar = cannon.GetChild(0).GetChild(0).GetComponent<Slider>();
     }
 
     void Update()
     {
+        //Rotates Cannon
         Vector3 turn = (playerTarget.transform.position - transform.position);
         turn.z = 0;
         cannon.transform.up = turn;
 
+        //Moves Cannon
         Vector2 delta = currentGizmo - (Vector2)transform.position;
 
         if (delta.magnitude <= .1f)
-            Debug.Log("Put Coroutine here");
+            StartCoroutine(WaitToMove());
+
+        UpdateCurrentGizmo();
 
         delta = delta.normalized;
         transform.position += (Vector3)delta * speed * Time.deltaTime;
@@ -38,15 +53,36 @@ public class Boss : MonoBehaviour
         currentGizmo = Globals.waypointGizmos.waypoints[currentIndex] + Random.insideUnitCircle;
     }
 
-    IEnumerable WaitToMove()
+    IEnumerator WaitToMove()
     {
-        if (currentIndex > 5)
+        if (currentIndex >= 4)
             currentIndex = 0;
         else
             currentIndex++;
 
-        UpdateCurrentGizmo();
-
-        yield return new WaitForSeconds(1.25f);
+        yield return new WaitForSeconds(15.00f);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == sword)
+        {
+            Debug.Log("Sword Hit");
+            //currentHealth -= swordDamage;
+            //StartCoroutine(Hit(currentHealth));
+        }
+       // else if (collision.gameObject == gun)
+       //     Debug.Log("Gun Hit");
+    }
+
+    //IEnumerator Hit(float health)
+    //{
+    //    healthBar.value = health / maxHealth;
+    //
+    //    cannon.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+    //
+    //    yield return new WaitForSeconds(3f);
+    //
+    //    cannon.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    //}
 }
