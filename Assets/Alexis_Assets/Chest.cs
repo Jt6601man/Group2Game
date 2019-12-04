07a1192a5521291ce7b2661524dd1e91820f3619
alexisBelt <57182633+alexisBelt@users.noreply.github.com>
2019-12-03 23:09:04 -0500

    Tilemaps Done
    Tilemaps done for world and the two dungeons and added colliders

---
+++ Assets/Alexis_Assets/Chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public GameObject openChest;
+
+    Transform chestPosition;
+
+    static bool enemyDead;
+
+    void Start()
+    {
+        chestPosition = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyDead)
+        {
+            Instantiate(openChest, chestPosition.position, chestPosition.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Alexis_Assets/Stairs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stairs : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(gameObject.tag == "toWorld")
+            SceneManager.LoadScene("OpenWorld");
+        else if(gameObject.tag == "toBoss")
+            SceneManager.LoadScene("BossDungeon");
+        else if(gameObject.tag == "toDungeon")
+            SceneManager.LoadScene("RegularDungeon");
+    }
+}
