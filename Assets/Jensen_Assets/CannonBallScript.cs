19e6430db83a90c40bf6605989485219566aa7e4
Jt6601man <jensen.tucker@gmail.com>
2019-12-05 23:51:50 -0500

    Added Cannonball script

---
+++ Assets/Jensen_Assets/CannonBallScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallScript : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Invoke("Die", 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(3, 0);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
